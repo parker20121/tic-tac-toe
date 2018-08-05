40092d8c76a272a8712b154ad031e022ee66ad40
Matt Parker <matt.parker@endoxa-us.com>
2018-08-04 20:29:19 -0400

    completed first rev of the board

--- board.py
+++ board.py
@@ -1,49 +1,50 @@
 
 class Board:
 
-    EMPTY = [None, None, None, None, None, None, None, None, None]
+	EMPTY = [None, None, None, None, None, None, None, None, None]
 
-    WINNING_POSITIONS = [(0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6)]
+	WINNING_POSITIONS = [(0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6)]
 
-    def __init__(self):
-        self.board = Board.EMPTY
+	def __init__(self):
+		self.board = Board.EMPTY
 
-    def move( self, player, position ):
-        self.board[position] = player
+	def move( self, player, position ):
+		self.board[position] = player
 
-    def reset(self):
-        self.board = Board.EMPTY
+	def reset(self):
+		self.board = Board.EMPTY
 
-    def is_winner(self, t):
-        v1 = self.board[t(0)]
-        v2 = self.board[t(1)]
-        v3 = self.board[t(2)]
+	def is_winner(self, t):
+		v1 = self.board[t[0]]
+		v2 = self.board[t[1]]
+		v3 = self.board[t[2]]
 
-        if v1 == v2 and v2 == v3 and v1 is not None:
-            return True
+		if v1 == v2 and v2 == v3 and v1 is not None:
+			return True
 
-        return False
+		return False
 
-    def has_winner(self):
-        for index, tuple in Board.WINNING_POSITIONS:
-            if self.is_winner( tuple ):
-                return index
-        return -1
+	def has_winner(self):
+		for index in range(8):
+			positions = Board.WINNING_POSITIONS[index]
+			if self.is_winner( positions ):
+				return index
+		return -1
 
-    def get_winner(self, index):
+	def get_winner(self, index):
 		winning_tuple = Board.WINNING_POSITIONS[index]
 		return self.board[ winning_tuple[0] ]
 
 	def empty_positions(self):
 		positions = []
-		for index in range(0, 8):
+		for index in range(9):
 			if self.board[index] is None:
 				positions.append(index)
 		return positions
 
-    def print(self):
+	def show(self):
 		return " {0} | {1} | {2} \n" \
-		       "===|===|===\n" \
+			   "===|===|===\n" \
 			   " {3} | {4} | {5} \n" \
 			   "===|===|===\n" \
 			   " {6} | {7} | {8} \n".format( *self.board )
--- board_test.py
+++ board_test.py
@@ -9,7 +9,26 @@ class BoardTest(unittest.TestCase):
         board_state = ['x', 'x', 'x', None, None, None, None, None, None]
         b = Board()
         b.board = board_state
-        self.assertTrue( b.check_winner() )
+        self.assertNotEqual( -1, b.has_winner() )
+
+    def test_move(self):
+        b = Board()
+        b.move("X", 0)
+        b.move("X", 1)
+        b.move("X", 2)
+        self.assertNotEqual( -1, b.has_winner() )
+
+    def test_empty_positons(self):
+        b = Board()
+        b.move("X", 0)
+        b.move("O", 1)
+        b.move("X", 4)
+        # print b.empty_positions()
+        self.assertEqual( 6, len(b.empty_positions()) )
+
+    def test_has_no_wineer(self):
+        b = Board()
+        self.assertEqual( -1, b.has_winner() )
 
 
 if __name__ == '__main__':
