5ccea9101b7b04652f06fcc52a45bb4063bbf09d
Matt Parker <matt.parker@endoxa-us.com>
2018-08-06 00:32:56 -0400

    Start of RL player

--- rl_player.py
+++ rl_player.py
@@ -1,7 +1,13 @@
 from player import Player
 
+import random
+
 
 class RLPlayer(Player):
+
+    LEARNING_RATE = 0.1
+    NO_ACTIONS = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    UNIFORM_ACTIONS = [1.0/9.0] * 9
 
     def __init__(self, name):
         Player.__init__(self, name)
@@ -12,15 +18,31 @@ class RLPlayer(Player):
         self.move_history = []
 
     def learn(self, reward):
+
+        for (state, next_move) in self.move_history:
+
         return
 
     def next_move(self, board, open_positions):
         state = board.state()
+        actions = []
 
         if state in self.memory:
-
+            actions = self.memory[ state ]
         else:
-            memory[ state ] = []
+            actions = RLPlayer.NO_ACTIONS
+            number_moves = len(open_positions)
 
-        return 0
+            if number_moves > 0:
+                for index in open_positions:
+                    actions[index] = 1.0/number_moves
+
+                self.memory[ state ] = actions
+            else:
+                self.memory[ state ] = RLPlayer.UNIFORM_ACTIONS
+
+        next_move = random.choice( open_positions )
+        self.move_history.append( (state, next_move) )
+
+        return next_move
 
